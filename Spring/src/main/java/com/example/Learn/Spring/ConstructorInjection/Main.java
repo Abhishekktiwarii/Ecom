0e3ef77496e31ae01cba665fc835e9e38d849a63
Abhishek <tiwariabhishek2409@gmail.com>
2025-10-08 17:37:06 +0530

    All about Constructor Injection

---
+++ Spring/src/main/java/com/example/Learn/Spring/ConstructorInjection/Main.java
@@ -0,0 +1,15 @@
+package com.example.Learn.Spring.ConstructorInjection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Car car = (Car) context.getBean("cars");
+        car.displayDetails();
+    }
+
+
+}
---
+++ Spring/src/main/java/com/example/Learn/Spring/ConstructorInjection/Specification.java
@@ -0,0 +1,29 @@
+package com.example.Learn.Spring.ConstructorInjection;
+
+public class Specification {
+    private String brand;
+    private String model;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+}
