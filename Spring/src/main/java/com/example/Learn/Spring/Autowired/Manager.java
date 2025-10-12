679d67003a544ce0b75bf311e4dfb3d1cd090659
Abhishek Tiwari <tiwariabhishek2409@gmail.com>
2025-10-12 22:02:41 +0530

    Autowired

--- Spring/src/main/java/com/example/Learn/Spring/Autowired/Manager.java
+++ Spring/src/main/java/com/example/Learn/Spring/Autowired/Manager.java
@@ -1,6 +1,7 @@
 package com.example.Learn.Spring.Autowired;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +9,7 @@ public class Manager {
 
 //    field injection
     @Autowired
+//    @Qualifier("employee") // it is used where there are several beans of same name.
     private Employee employee;
 
 
